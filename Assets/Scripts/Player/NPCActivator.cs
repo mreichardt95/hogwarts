5559ccc2d284273772ddace12ad2fb2eff316d66
muertet <fp557s@gmail.com>
2016-04-25 19:20:44 +0200

    = NPC test

--- Assets/Scripts/Player/NPCActivator.cs
+++ Assets/Scripts/Player/NPCActivator.cs
@@ -3,9 +3,6 @@ using System.Collections;
 
 public class NPCActivator : MonoBehaviour {
 
-	void Start(){
-	}
-
 	void OnTriggerEnter(Collider col){
 		if ((col.tag == "NPC")&&(!col.isTrigger)) {
 			if (col.gameObject.GetComponent<PhotonView> ().owner == null) {
@@ -13,7 +10,7 @@ public class NPCActivator : MonoBehaviour {
 				col.gameObject.GetComponent<PhotonView> ().TransferOwnership (PhotonNetwork.player);
 			}
 			col.gameObject.GetComponent<Animation> ().enabled = true;
-			//col.gameObject.GetComponent<NPC> ().enabled = true;
+			col.gameObject.GetComponent<NPC> ().enabled = true;
 			col.gameObject.transform.FindChild ("Model").gameObject.SetActive (true);
 		}
 	}
@@ -23,7 +20,7 @@ public class NPCActivator : MonoBehaviour {
 			Debug.Log ("Now the scene owns the object, previous owner was "+col.gameObject.GetComponent<PhotonView> ().owner.name);
 			col.gameObject.GetComponent<PhotonView> ().TransferOwnership (PhotonNetwork.masterClient);
 			col.gameObject.GetComponent<Animation> ().enabled = false;
-			//col.gameObject.GetComponent<NPC> ().enabled = false;
+			col.gameObject.GetComponent<NPC> ().enabled = false;
 			col.gameObject.transform.FindChild ("Model").gameObject.SetActive (false);
 		}
 	}
